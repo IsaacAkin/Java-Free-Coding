3d575a60b2dbbc60c6d7cd0168c0013469981701
Halceeyon <IsaacAki247@gmail.com>
2022-08-05 16:52:24 +0100

    Built up PetrolPurchace class
     - Added instance variables and initialized them in a constructor.
    - Created getters and setters
    - Created getPurchaseAmount method to calculate the purchase amount for petrol that will be used in another class (Petrol)

--- src/PetrolPurchase.java
+++ src/PetrolPurchase.java
@@ -1,2 +1,62 @@
 public class PetrolPurchase {
+
+    private String stationLocation;
+    private String petrol;
+    private int quantityInLitres;
+    private double pricePerLitre;
+    private double percentageDiscount;
+
+    public PetrolPurchase(String stationLocation, String petrol, int quantityInLitres, double pricePerLitre, double percentageDiscount) {
+
+        this.stationLocation = stationLocation;
+        this.petrol = petrol;
+        this.quantityInLitres = quantityInLitres;
+        this.pricePerLitre = pricePerLitre;
+        this.percentageDiscount = percentageDiscount;
+    }
+
+    public double getPurchaseAmount(double purchaseAmount) {
+        purchaseAmount = quantityInLitres * pricePerLitre - percentageDiscount;
+        return purchaseAmount;
+    }
+
+    public String getStationLocation() {
+        return stationLocation;
+    }
+
+    public void setStationLocation(String stationLocation) {
+        this.stationLocation = stationLocation;
+    }
+
+    public String getPetrol() {
+        return petrol;
+    }
+
+    public void setPetrol(String petrol) {
+        this.petrol = petrol;
+    }
+
+    public int getQuantityInLitres() {
+        return quantityInLitres;
+    }
+
+    public void setQuantityInLitres(int quantityInLitres) {
+        this.quantityInLitres = quantityInLitres;
+    }
+
+    public double getPricePerLitre() {
+        return pricePerLitre;
+    }
+
+    public void setPricePerLitre(double pricePerLitre) {
+        this.pricePerLitre = pricePerLitre;
+    }
+
+    public double getPercentageDiscount() {
+        return percentageDiscount;
+    }
+
+    public void setPercentageDiscount(double percentageDiscount) {
+        this.percentageDiscount = percentageDiscount;
+    }
 }
