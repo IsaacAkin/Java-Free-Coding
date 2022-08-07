f187cdcd652b7a1628e786b940592f66d2be9e19
Halceeyon <IsaacAki247@gmail.com>
2022-08-07 14:41:21 +0100

    populated CarApplication.java
    - added code to the class

--- src/CarApplication.java
+++ src/CarApplication.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class CarApplication {
 
     public static void main(String[] args) {
 
+        String model;
+        String year;
+        double price;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("");
+        model = input.nextLine();
+
+        System.out.println("");
+        year = input.nextLine();
+
+        System.out.println("");
+        price = input.nextDouble();
+
+        Car car1 = new Car(model, year, price);
+        Car car2 = new Car(model, year, price);
+
+        double price1 = car1.getPrice();
+        double price2 = car2.getPrice();
+
+        double discount1 = price1 / 0.5;
+        double discount2 = price2 / 0.7;
+
+        //System.out.printf("", discount1.getPrice());
+        System.out.printf("");
 
     }
 }
