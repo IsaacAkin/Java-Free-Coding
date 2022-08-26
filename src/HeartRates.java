546d369cbdf96d5270003e8c4fb4129804740853
Halceeyon <IsaacAki247@gmail.com>
2022-08-26 02:23:32 +0100

    Created HeartRates.java
    Populated class with code

---
+++ src/HeartRates.java
@@ -0,0 +1,69 @@
+public class HeartRates {
+
+    private String firstName;
+    private String lastName;
+    private int day;
+    private int month;
+    private int year;
+
+    public HeartRates(String firstName, String lastName, int day, int month, int year) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int ageInYears() {
+
+    }
+
+    public double maximumHeartRate() {
+
+    }
+
+    public double targetHeartRate() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
